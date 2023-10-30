bfb28b68bb0e284a697bb79b71b2183053a2d3b9
jurmaev <smartemii03@gmail.com>
2023-10-30 22:26:00 +0500

    add reset functionality

--- src/js/main.js
+++ src/js/main.js
@@ -5,21 +5,18 @@
   const startBtn = document.querySelector('.btn');
   const controls = document.querySelector('.controls');
 
-  const lapResetBtn = document.createElement('button');
-  lapResetBtn.classList.add('btn');
-  lapResetBtn.textContent = 'Lap';
-  lapResetBtn.id = 'lap';
-
-  const pauseResumeBtn = document.createElement('button');
-  pauseResumeBtn.classList.add('btn');
-  pauseResumeBtn.textContent = 'Pause';
-  pauseResumeBtn.id = 'pause';
+  function getTime(milliseconds) {
+    const minutes = parseInt(milliseconds / 1000 / 60);
+    const seconds = parseInt(milliseconds / 1000);
+    const decimal = parseInt((milliseconds % 1000) / 10);
+    return `${minutes < 10 ? '0' + minutes : minutes}:${
+      seconds < 10 ? '0' + seconds : seconds
+    }.${decimal < 10 ? '0' + decimal : decimal}`;
+  }
 
   function startInterval() {
     const id = setInterval(() => {
-      stopwatch.textContent = `${parseInt(milliseconds / 1000 / 60)}:${parseInt(
-        milliseconds / 1000
-      )}.${parseInt((milliseconds % 1000) / 10)}`;
+      stopwatch.textContent = getTime(milliseconds);
       milliseconds += 10;
     }, 10);
     return id;
@@ -27,28 +24,43 @@
 
   startBtn.addEventListener('click', () => {
     intervalId = startInterval();
+
+    const lapResetBtn = document.createElement('button');
+    lapResetBtn.classList.add('btn');
+    lapResetBtn.textContent = 'Lap';
+    lapResetBtn.id = 'lap';
+
+    const pauseResumeBtn = document.createElement('button');
+    pauseResumeBtn.classList.add('btn');
+    pauseResumeBtn.textContent = 'Pause';
+    pauseResumeBtn.id = 'pause';
+
     controls.innerHTML = '';
     controls.append(lapResetBtn);
     controls.append(pauseResumeBtn);
 
     lapResetBtn.addEventListener('click', () => {
       if (lapResetBtn.id === 'lap') {
-        // lapResetBtn.textContent = 'Reset';
-        // lapResetBtn.id = 'reset';
       } else if (lapResetBtn.id === 'reset') {
-        clearInterval(intervalId);
         milliseconds = 0;
         stopwatch.textContent = '00:00.00';
+        controls.innerHTML = '';
+        controls.append(startBtn);
+        clearInterval(intervalId);
       }
     });
 
     pauseResumeBtn.addEventListener('click', () => {
       if (pauseResumeBtn.id === 'pause') {
         clearInterval(intervalId);
+        lapResetBtn.textContent = 'Reset';
+        lapResetBtn.id = 'reset';
         pauseResumeBtn.textContent = 'Resume';
         pauseResumeBtn.id = 'resume';
       } else if (pauseResumeBtn.id === 'resume') {
         intervalId = startInterval();
+        lapResetBtn.textContent = 'Lap';
+        lapResetBtn.id = 'lap';
         pauseResumeBtn.textContent = 'Pause';
         pauseResumeBtn.id = 'pause';
       }
