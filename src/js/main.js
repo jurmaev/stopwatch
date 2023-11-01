fbbd3fff3008223715828f2ee3b78b334cae766d
jurmaev <smartemii03@gmail.com>
2023-11-01 22:58:40 +0500

    add laps functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,23 @@
 (() => {
   let milliseconds = 0;
   let intervalId;
+  let laps = 0;
+  let previousLap = 0;
+
   const stopwatch = document.querySelector('.stopwatch');
   const startBtn = document.querySelector('.btn');
   const controls = document.querySelector('.controls');
+  const lapsContainer = document.querySelector('.laps');
+
+  function getNumber(number) {
+    return number < 10 ? '0' + number : number;
+  }
 
   function getTime(milliseconds) {
     const minutes = parseInt(milliseconds / 1000 / 60);
     const seconds = parseInt(milliseconds / 1000);
     const decimal = parseInt((milliseconds % 1000) / 10);
-    return `${minutes < 10 ? '0' + minutes : minutes}:${
-      seconds < 10 ? '0' + seconds : seconds
-    }.${decimal < 10 ? '0' + decimal : decimal}`;
+    return `${getNumber(minutes)}:${getNumber(seconds)}.${getNumber(decimal)}`;
   }
 
   function startInterval() {
@@ -41,10 +47,31 @@
 
     lapResetBtn.addEventListener('click', () => {
       if (lapResetBtn.id === 'lap') {
+        const lapsItem = document.createElement('div');
+        const lapsNumber = document.createElement('span');
+        const lapsTime = document.createElement('span');
+        const lapsDiff = document.createElement('span');
+
+        lapsItem.classList.add('laps__item');
+        lapsNumber.classList.add('laps__number');
+        lapsTime.classList.add('laps__time');
+        lapsDiff.classList.add('laps__diff');
+
+        lapsNumber.textContent = getNumber(++laps);
+        lapsTime.textContent = getTime(milliseconds);
+        lapsDiff.textContent = `+${getTime(milliseconds - previousLap)}`;
+        previousLap = milliseconds;
+
+        lapsItem.append(lapsNumber);
+        lapsItem.append(lapsTime);
+        lapsItem.append(lapsDiff);
+        lapsContainer.prepend(lapsItem);
       } else if (lapResetBtn.id === 'reset') {
         milliseconds = 0;
+        laps = 0;
         stopwatch.textContent = '00:00.00';
         controls.innerHTML = '';
+        lapsContainer.innerHTML = '';
         controls.append(startBtn);
         clearInterval(intervalId);
       }
